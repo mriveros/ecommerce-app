96e73c2396b793bc7a86e1040ea2c2ac9a780837
Marcos Riveros <mriveros.py@gmail.com>
2018-06-22 10:34:39 -0400

    forders, files and codes change

---
+++ app/src/main/java/com/mriveros/ecommerceapp/analytics/AnalyticsConfig.java
@@ -0,0 +1,8 @@
+package com.mriveros.ecommerceapp.analytics;
+
+public class AnalyticsConfig {
+
+    // true for enabling Google Analytics, should be true in production release
+    public static final boolean ANALYTICS = true;
+
+}
