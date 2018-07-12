3b33246b5577fe545e17e17e1be97688d4ffc40e
Marcos Riveros <mriveros.py@gmail.com>
2018-07-12 09:11:38 -0400

    adding register with more values for the client

--- app/src/main/java/com/mriveros/ecommerceapp/RegisterActivity.java
+++ app/src/main/java/com/mriveros/ecommerceapp/RegisterActivity.java
@@ -23,19 +23,34 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //final EditText etAge = (EditText) findViewById(R.id.etAge);
+
         final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etLastname = (EditText) findViewById(R.id.etLastname);
+        final EditText etBirthdate = (EditText) findViewById(R.id.etBirthdate);
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etAddress = (EditText) findViewById(R.id.etAddress);
+        final EditText etCity = (EditText) findViewById(R.id.etCity);
+        final EditText etBarrio = (EditText) findViewById(R.id.etBarrio);
+        final EditText etPhone = (EditText) findViewById(R.id.etPhone);
+        final EditText etMail = (EditText) findViewById(R.id.etMail);
+
         final Button bRegister = (Button) findViewById(R.id.bRegister);
 
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String name = etName.getText().toString();
+                final String lastname = etLastname.getText().toString();
+                final String birtdate = etBirthdate.getText().toString();
                 final String username = etUsername.getText().toString();
-               // final int age = Integer.parseInt(etAge.getText().toString());
                 final String password = etPassword.getText().toString();
+                final String address = etAddress.getText().toString();
+                final String city = etCity.getText().toString();
+                final String neigborhood = etBarrio.getText().toString();
+                final String phone = etPhone.getText().toString();
+                final String mail = etMail.getText().toString();
+
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
@@ -59,7 +74,7 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 };
 
-                RegisterRequest registerRequest = new RegisterRequest(name, username, 12, password, responseListener);
+                RegisterRequest registerRequest = new RegisterRequest(name, lastname, birtdate, username, password, address, city, neigborhood, phone, mail, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
--- app/src/main/java/com/mriveros/ecommerceapp/RegisterRequest.java
+++ app/src/main/java/com/mriveros/ecommerceapp/RegisterRequest.java
@@ -10,11 +10,10 @@ public class RegisterRequest extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "http://www.smarthub.design/ecommerce/register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener) {
+    public RegisterRequest(String name,String lastname, String birtdate,String username, String password,String address, String city, String neigborhood, String phone, String mail, Response.Listener<String> listener) {
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("name", name);
-        params.put("age", age + "");
         params.put("username", username);
         params.put("password", password);
     }
